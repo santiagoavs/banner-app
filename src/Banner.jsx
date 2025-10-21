6c23e71e9732d9ae172a4e0338d68f826772629a
santiagoavs <santiago4v5@gmail.com>
2025-10-21 17:42:21 -0600

    initial commit

---
+++ src/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="gradient-bg"></div>
+      <div className="content">
+        <h1 className="title">Santiago Ávila</h1>
+        <p className="subtitle">Full Stack Developer</p>
+        <p className="description">Nivel avanzado en inglés (C1)</p>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Banner from './Banner'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Banner />
+  </React.StrictMode>,
+)
